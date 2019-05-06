0485d9d0cac252e2fc1953ab63a820886cd031f6
trigaten <sanderman5@hotmail.com>
2019-05-06 13:51:33 -0400

    fix $this

--- searchObject.php
+++ searchObject.php
@@ -4,26 +4,25 @@ class searchObject {
 
     private $query;
     private $queryWords;
-    private $arrayGetter;
     private $plantingLog;
     private $plantCharacteristics;
 
     function __construct() {
 
         $arrayGetter = new arrayDownloader(); 
-        $plantingLog = $arrayGetter->getPlantingLog();
-        $plantCharacteristics = $arrayGetter->getPlantCharacteristics();
-        $query = "Plantanus Robbinus";
-        $queryWords = explode(" ", $query);
+        $this->plantingLog = $arrayGetter->getPlantingLog();
+        $this->plantCharacteristics = $arrayGetter->getPlantCharacteristics();
+        $this->query = "Plantanus Robbinus";
+        $this->queryWords = explode(" ", $this->query);
 
     }
 
     function stringSearch($query){
-
-        $queryWords = explode(" ", $query);
+        $this->query = $query;
+        $this->queryWords = explode(" ", $query);
         $rowsFound = array();
 
-        foreach ($plantingLog as $row) {
+        foreach ($this->plantingLog as $row) {
             if ($row[1] == $_GET["Name"]){
             for ($i = 0; $i < sizeof($row); $i++) {
                 if ($i != 1 && $i != 9 && $row[$i] != null){
@@ -36,7 +35,7 @@ class searchObject {
             
         }
 
-        foreach ($plantCharacteristics as $row) {
+        foreach ($this->plantCharacteristics as $row) {
             if ($row[1] == $_GET["Name"]){
             for ($i = 0; $i < sizeof($row); $i++) {
                 if ($i != 1 && $i != 9 && $row[$i] != null){
