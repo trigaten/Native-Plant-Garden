c28a27f1f68afc693efb88ca8dc7df388195f035
jguallar-blasco <jguallar-blasco@friendsbalt.org>
2019-05-06 20:53:58 -0400

    as

--- Lvl1PlantDisplay.php
+++ Lvl1PlantDisplay.php
@@ -27,13 +27,5 @@ $plantname = $plant;
         echo "<td>$wetlandStatus</td>";
         echo "<td>$endangered</td>";
         echo "</tr>";}
-
-
-
-// <tr></tr><td>&nbsp;&nbsp;&nbsp;Common Name: <?php echo escape($rowS["CommonName"]);?></td>
-// <tr></tr><td>&nbsp;&nbsp;&nbsp;Bloom Time: <?php echo escape($rowS["BloomTime"]); ?></td>
-// <tr></tr><td>&nbsp;&nbsp;&nbsp;Wetland Status: <?php echo escape($rowS["WetlandStatus"])?></td>
-// <tr></tr><td>&nbsp;&nbsp;&nbsp;Endangered: <?php echo escape($rowS["Endangered"])?></td>
-// <tr></tr><td>&nbsp;&nbsp;&nbsp;<a href="<?php echo escape($rowS["Link"]); ?>">Click here for more information</a><br><br></td> </tr>
-       
+  
 ?>
