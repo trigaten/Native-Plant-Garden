b844eba1adc6312c3d53fdf64429497544d9e131
jguallar-blasco <jguallar-blasco@friendsbalt.org>
2019-05-06 21:51:17 -0400

    cs

--- Lvl1PlantDisplay.php
+++ Lvl1PlantDisplay.php
@@ -1,4 +1,44 @@
+<?php 
+?><html><meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
 
+/* Modal Content */
+.modal-content {
+  background-color: #fefefe;
+  margin: auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+
+/* The Close Button */
+.close {
+  color: #aaaaaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: #000;
+  text-decoration: none;
+  cursor: pointer;
+}
+</style>
 
 <?php
 //.gets location of plant in spreadsheet
@@ -12,16 +52,68 @@ $bloomTime = "yamyamyam";
 $wetlandStatus = "yam^4";
 $endangered = "yam^5";
 
-echo($family);{
-        echo ("<td>$plantname</td>");
-        echo ("<td>$latinname</td>");
+        echo "<table>"; 
+        echo "</tr>"; 
+        echo ("<td>$plantName</td>");
+        echo ("<td>$latinName</td>");
         echo ("<td>$family</td>");
-        echo ("<td>$growthform</td>");
+        echo ("<td>$growthForm</td>");
         echo ("<td>$bloomTime</td>");
         echo ("<td>$wetlandStatus</td>");
         echo ("<td>$endangered</td>");
+        ?><td><!-- Trigger/Open The Modal -->
+        <button id="myBtn">Open Modal</button>
+        
+        <!-- The Modal -->
+        <div id="myModal" class="modal">
+        
+          <!-- Modal content -->
+          <div class="modal-content">
+            <span class="close">&times;</span>
+            <p><?php echo "<table>"; 
+                     echo "</tr>"; 
+                     echo ("<td>$plantName</td>");
+                     echo ("<td>$latinName</td>");
+                     echo ("<td>$family</td>");
+                     echo ("<td>$growthForm</td>");
+                     echo ("<td>$bloomTime</td>");
+                     echo ("<td>$wetlandStatus</td>");
+                     echo ("<td>$endangered</td>");?></p>
+          </div>
+        
+        </div>
+        <script>
+        // Get the modal
+        var modal = document.getElementById('myModal');
+
+        // Get the button that opens the modal
+        var btn = document.getElementById("myBtn");
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks the button, open the modal 
+        btn.onclick = function() {
+        modal.style.display = "block";
+}
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+        modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+        if (event.target == modal) {
+        modal.style.display = "none";
+        }
+        }
+        </script>
+        </td><?php
         echo "</tr>";
-    }
+        echo "<table>"; 
+
+   
 
 //include "getImage.php";
 //include "printImage.php";
