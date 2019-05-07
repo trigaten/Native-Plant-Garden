103f3f70a46e9b72d55158da356eb26a2fda2f5f
jguallar-blasco <jguallar-blasco@friendsbalt.org>
2019-05-06 21:23:18 -0400

    cs

--- Lvl1PlantDisplay.php
+++ Lvl1PlantDisplay.php
@@ -12,14 +12,7 @@ $bloomTime = "yamyamyam";
 $wetlandStatus = "yam^4";
 $endangered = "yam^5";
 
-echo($family); 
-
-//include "getImage.php";
-//include "printImage.php";
-//function displayLvl1($plant){
-//$plantname = $plant; 
-//echo $directory_plantname;
-    echo "<table style='width:100%'><tr>";
+echo($family);{
         echo ("<td>$plantname</td>");
         echo ("<td>$latinname</td>");
         echo ("<td>$family</td>");
@@ -27,6 +20,14 @@ echo($family);
         echo ("<td>$bloomTime</td>");
         echo ("<td>$wetlandStatus</td>");
         echo ("<td>$endangered</td>");
-        echo "</tr>";}; 
+        echo "</tr>";
+    }
+
+//include "getImage.php";
+//include "printImage.php";
+//function displayLvl1($plant){
+//$plantname = $plant; 
+//echo $directory_plantname;    
   
 ?>
+
