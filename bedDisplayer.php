b1b9c347ffadb91b7021f41849bbfb092e969a69
jguallar-blasco <jguallar-blasco@friendsbalt.org>
2019-05-13 13:06:10 -0400

    as

--- bedDisplayer.php
+++ bedDisplayer.php
@@ -2,13 +2,14 @@
 include "searchObject.php";
 include "Lvl1PlantDisplay.php"; 
 
-$bedPictures = array(); //array of pictures of the bed
+//$bedPictures = array(); //array of pictures of the bed
 
-$title = "staircase";
+$title = "Staircase";
 
 $searcher = new SearchObject(); 
 $searchArray = $searcher->bedSearchByName($title); //Creates array of all Latin Names in specific garden
-echo(sizeof($searchArray)); 
+echo(count($searchArray));
+echo("hi");
 
 //$currentPlant = $searchArray[1]; 
 //echo($currentPlant); 
