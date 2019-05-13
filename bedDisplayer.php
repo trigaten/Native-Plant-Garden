fe13eeb181236431ea554a336f08babea8c99a21
jguallar-blasco <jguallar-blasco@friendsbalt.org>
2019-05-13 19:46:51 -0400

    as

--- bedDisplayer.php
+++ bedDisplayer.php
@@ -1,18 +1,15 @@
 <?php
+
+include "printImage.php";
+//include "arrayDownloader.php";
 include "searchObject.php";
-include "Lvl1PlantDisplay.php"; 
+include "Lvl1PlantDisplay.php";
 
-//$bedPictures = array(); //array of pictures of the bed
-
-$title = "Staircase";
-
-$searcher = new SearchObject(); 
-$searchArray = $searcher->bedSearchByName($title); //Creates array of all Latin Names in specific garden
-echo(count($searchArray));
-echo("hi");
-
-//$currentPlant = $searchArray[1]; 
-//echo($currentPlant); 
-
+$name = "Forbush Memorial"; 
+    
+$Object = new searchObject(); 
+$values = $Object->bedSearchByName($name); 
+  
+echo(lengthof($values)); 
 
 ?>
