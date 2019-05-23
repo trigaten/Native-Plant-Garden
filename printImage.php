4c3a639765cd06fd7cf2a1b5bb65dd0bd5f96e2c
trigaten <sanderman5@hotmail.com>
2019-05-23 15:14:52 -0400

    deleteplants

--- printImage.php
+++ printImage.php
@@ -2,17 +2,17 @@
 include "getImage.php";
 function printImage($plant){
 $plantname = $plant;
-$directory_plantname = 'images/'.$plantname.".jpg";
+$directory_plantname = 'images/plantImages/'.$plantname.".jpg";
 //echo $directory_plantname;
 if (file_exists($directory_plantname)) {
 return $directory_plantname;//'<img src="' . $directory_plantname . '" alt="error">'; 
      } 
     else{
         $link =  getUrl($plantname);
-        $destdir = 'images/';
+        $destdir = 'images/plantImages';
         $img=file_get_contents($link);
         file_put_contents($destdir.substr($plantname . ".jpg", strrpos("hahaha",'/')), $img); //idek
-        $imgloc = "images/$plantname".".jpg";
+        $imgloc = "images/plantImages/$plantname".".jpg";
         // echo '<img src="' . $imgloc . '" alt="error">'; 
         return $imgloc; 
     }
